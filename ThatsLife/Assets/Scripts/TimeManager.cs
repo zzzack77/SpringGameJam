30977be9e89f22b6b0562e4f48cb6e82e85ed20a
SilverXOmega <bwlego@outlook.com>
2026-04-12 14:56:12 +0100

    Added time and day tracking

--- ThatsLife/Assets/Scripts/TimeManager.cs
+++ ThatsLife/Assets/Scripts/TimeManager.cs
@@ -1,16 +1,33 @@
 using UnityEngine;
 
+public enum Season
+{
+    Spring,
+    Summer,
+    Autumn,
+    Winter
+}
 public class TimeManager : MonoBehaviour
 {
+    public Season currentSeason;
+    // Day Length is how many seconds each day will be
+    private float dayLength = 15f;
+    private float totalGameTime;
+    // How many days have passed, starts on 79 as that is the start of spring
+    private float totalDays;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        currentSeason = Season.Spring;
+        totalGameTime = dayLength * 79;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        totalGameTime += Time.deltaTime;
+        totalDays = totalGameTime / dayLength;
+        int dayNumber = Mathf.FloorToInt(totalDays);
+        Debug.Log(dayNumber);
     }
 }
